91d9c009ada10234cd29cdb1c9b392b54c92aeda
James Read <james.read.18@gmail.com>
2024-06-29 12:05:13 +0800

    feat: adding tests strct for JwtAuthUpgradeRector
    Adding in file structure for testing JwtAuthUpgradeRector for upgrade
    from v1 to v2

---
+++ tests/Rector/JwtAuthUpgradeRector/JwtAuthUpgradeRectorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JimTools\JwtAuth\Test\Rector\JwtAuthUpgradeRector;
+
+use Iterator;
+use JimTools\JwtAuth\Rector\JwtAuthUpgradeRector;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+#[CoversClass(JwtAuthUpgradeRector::class)]
+class JwtAuthUpgradeRectorTest extends AbstractRectorTestCase
+{
+
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/config.php';
+    }
+}
